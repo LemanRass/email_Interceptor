30002aab06aecef27a9e824414a05edf787bb845
LemanRass <lemanrass18@gmail.com>
2020-10-17 15:07:19 +0300

    Initial setup

---
+++ project/main.js
@@ -0,0 +1,7 @@
+var gmail = require("node-gmail-api");
+
+let config = {
+  clientID: "393177752362-u4c3iuvksiv21i8k92hrjf1e1n7esul2.apps.googleusercontent.com",
+  clientSecret: "rgwiXlEnUccy_v1TqAbCmmn4"
+};
+
